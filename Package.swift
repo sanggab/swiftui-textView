4eb5036527ac15a41743bedde63fe1cee97ccc38
심상갑 <kappa2410@gmail.com>
2024-07-26 17:11:28 +0900

    Feat: 초기세팅

--- Package.swift
+++ Package.swift
@@ -9,16 +9,16 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "swiftui-textView",
-            targets: ["swiftui-textView"]),
+            name: "GabTextView",
+            targets: ["GabTextView"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "swiftui-textView"),
+            name: "GabTextView"),
         .testTarget(
-            name: "swiftui-textViewTests",
-            dependencies: ["swiftui-textView"]),
+            name: "GabTextViewTests",
+            dependencies: ["GabTextView"]),
     ]
 )
